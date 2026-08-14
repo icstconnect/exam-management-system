44b63d7956df387d428e4c9ab17156d5969bad4f
ICST Chowberia <icstlab@outlook.com>
2026-08-14 13:38:15 +0530

    chore: remove temporary test scripts and outdated sql artifacts

--- backend/check_db.js
+++
@@ -1,3 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: 'postgresql://postgres:postgres@localhost:5433/exam_db' });
-pool.query("SELECT * FROM students WHERE batch = 'V,VI Batch 1'").then(res => console.log('V,VI Batch 1:', res.rows.length)).catch(console.error).finally(() => pool.end());
--- backend/check_exams.js
+++
@@ -1,3 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: 'postgresql://postgres:postgres@localhost:5433/exam_db' });
-pool.query("SELECT exam_id, title, target_batch, status FROM exams").then(res => console.log(res.rows)).catch(console.error).finally(() => pool.end());
